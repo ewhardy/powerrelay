a9058e258ca1ab3623f3629a686c206e894a2d67
Ted Hardy <ted@hardyriles.com>
2025-11-21 18:35:03 -0800

    Updated Get-PowerRelayControllerInformation.ps1 -Corrected path for ControllerInformation URI Path

--- src/ps/Get-PowerRelayControllerInformation.ps1
+++ src/ps/Get-PowerRelayControllerInformation.ps1
@@ -23,21 +23,50 @@ function Get-PowerRelayControllerInformation {
         })]
         [String]$powerRelayIp = "192.168.112.150",
         [ValidateScript({Test-path -Path $_})]
-        [string]$SecretsFilePath = $env:PowerRelaySecretPath
+        [string]$SecretsFilePath = $env:PowerRelaySecretPath,
+        [bool]$UseHttps = $false,
+        [bool]$TrustHttpsCert = $false
 )
+        $httpMethod = "http"
+        if ($UseHttps) {
+                $httpMethod = "https"
+        }
+        $RelayInfoResponse = $null
+
         $secretsfile = $SecretsFilePath
 
         $rawlogin = Get-Content -TotalCount 1 -Path $secretsfile
 
         $webLogin = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($rawlogin))
 
-        $GetRelayInfoUri = "http://$($powerRelayIp)/restapi/"
+        $outputUri =[System.UriBuilder]::new($httpMethod, $powerRelayIp)
+        $outputUri.Path = '/restapi/'
+
+        $GetRelayInfoUri = $outputUri.Uri.ToString()
+
+        Write-Debug "Api URI: $($GetRelayInfoUri)"
+
+        $apiStartTime = Get-date
+
+        if ($TrustHttpsCert) {
+                $RelayInfoResponse = Invoke-RestMethod  `
+                        -SkipCertificateCheck `
+                        -Method Get `
+                        -Headers @{Authorization = "Basic $webLogin" } `
+                        -Uri $GetRelayInfoUri
+
+        }
+        else {
+                $RelayInfoResponse = Invoke-RestMethod  `
+                        -Method Get `
+                        -Headers @{Authorization = "Basic $webLogin" } `
+                        -Uri $GetRelayInfoUri
+        }
 
 
-        $RelayInfoResponse = Invoke-RestMethod  `
-                -Method Get `
-                -Headers @{Authorization = "Basic $webLogin" } `
-                -Uri $GetRelayInfoUri
+        $apiDuration = "Api Duration:$((Get-date).Subtract($ApiStartTime))"
+        
+        Write-Debug $apiDuration
 
         $RelayInfoResponse
 }
